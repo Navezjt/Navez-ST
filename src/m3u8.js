63e71b0ee8021ae8f78b19c06a7e9f002501095b
abskmj <abskmj@gmail.com>
2022-10-19 07:32:40 +0000

    Add support for youtube video id

--- src/m3u8.js
+++ src/m3u8.js
@@ -28,7 +28,7 @@ for (const channel of channels) {
   playlist.write(`
 
 #EXTINF:-1 group-title="${channel.group}" tvg-language="${channel.language}" tvg-logo="${channel.logo}", ${channel.name}
-https://hls.youtb.workers.dev/channel/${channel.youtube}.m3u8`)
+https://hls.youtb.workers.dev/${channel.youtube}.m3u8`)
 }
 
 playlist.end()
